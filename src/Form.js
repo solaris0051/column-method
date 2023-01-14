cb1f419a4f7e86ddd29a11eeccfe482ed55498a9
solaris0051 <docodoco.0713@outlook.com>
2023-01-14 21:56:58 +0900

    2nd build

--- src/Form.js
+++ src/Form.js
@@ -8,34 +8,34 @@ export default function Form() {
         <input type="hidden" name="form-name" value="column-method" />
         <div>
           <label htmlFor="occasion">①状況 = Event</label><span>*</span> <br />
-          <textarea id="occasion" name="occasion" cols="50" rows="2" required></textarea>
+          <textarea id="occasion" name="occasion" cols="100" rows="3" required></textarea>
         </div>
         <div>
           <label htmlFor="feels">②気分 = Feels</label><span>*</span> <br />
           それぞれの強さを%表記(寄与の大きいものに着目) <br />
-          <textarea id="feels" name="feels" cols="50" rows="2" required></textarea>
+          <textarea id="feels" name="feels" cols="100" rows="3" required></textarea>
         </div>
         <div>
           <label htmlFor="schematic-idea">③自動思考 = Idea</label><span>*</span> <br />
-          <textarea id="schematic-idea" name="schematic-idea" cols="50" rows="2" required></textarea>
+          <textarea id="schematic-idea" name="schematic-idea" cols="100" rows="3" required></textarea>
         </div>
         <div>
           <label htmlFor="cause">④自動思考の根拠</label><span>*</span> <br />
-          <textarea id="cause" name="message" cols="50" rows="2" required></textarea>
+          <textarea id="cause" name="message" cols="100" rows="3" required></textarea>
         </div>
         <div>
           <label htmlFor="anti-these">⑤反証</label><span>*</span> <br />
           自己の経験に基づくものがよい。XXにとらわれすぎない。 (XX: 指標など) <br />
-          <textarea id="anti-these" name="anti-these" cols="50" rows="2" required></textarea>
+          <textarea id="anti-these" name="anti-these" cols="100" rows="3" required></textarea>
         </div>
         <div>
           <label htmlFor="rational-idea">⑥適応的思考</label><span>*</span> <br />
-          <textarea id="rational-idea" name="rational-idea" cols="50" rows="2" required></textarea>
+          <textarea id="rational-idea" name="rational-idea" cols="100" rows="3" required></textarea>
         </div>
         <div>
           <label htmlFor="change">⑦気分の変化</label><span>*</span> <br />
           それぞれの強さを%表記 <br />
-          <textarea id="change" name="change" cols="50" rows="2" required></textarea>
+          <textarea id="change" name="change" cols="100" rows="3" required></textarea>
         </div>
         <div>
           <input type="submit" value="送信" />
