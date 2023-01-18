def594334ff6ae17b7ee401cb484b2582ce78b81
solaris0051 <30798030+solaris0051@users.noreply.github.com>
2023-01-18 12:06:34 +0900

    Footer comp & CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./Footer";
 import Form from "./Form";
 
 function App() {
@@ -7,11 +8,12 @@ function App() {
       <h1> Column Method </h1>
       メタ認知を活かすためのTips
       <ul>
-        <li>①-③を順に整列します。<span>問題定義部 (状況の複雑さに応じて、よく整理します。)</span></li>
-        <li>④-⑦で問題を解いていきます。<span>問題解決部 / 低減部</span></li>
+        <li><u>①-③を順に整列します。</u>問題定義部 (状況の複雑さに応じて、よく整理します。)</li>
+        <li><u>④-⑦で問題を解いていきます。</u>問題解決部 / 低減部</li>
       </ul>
       <span>*</span>必須
       <Form />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        This product is dedicated to Ms. Watt since Jan, 2023. ©Y.K.
+      </div>
+    </footer>
+  );
+}
