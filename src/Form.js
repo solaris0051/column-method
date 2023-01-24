3bfdb1a3266d314b3166fd34396d9e787ba9ac29
solaris0051 <30798030+solaris0051@users.noreply.github.com>
2023-01-24 13:18:59 +0900

    lauout improved.

--- src/Form.js
+++ src/Form.js
@@ -12,35 +12,35 @@ export default function Form() {
       <p><span>*</span>必須</p>
       <div>
         <label htmlFor="occasion">①状況 = Event</label><span>*</span> <br />
-        <textarea id="occasion" name="occasion" cols="62" rows="4" placeholder="発生タイミングは、②③が発生する直前です。それを叙事的(機械的)に書きます。" required></textarea>
+        <textarea id="occasion" name="occasion" cols="50" rows="4" placeholder="発生タイミングは、②③が発生する直前です。それを叙事的(機械的)に書きます。" required></textarea>
       </div>
       <div>
         <label htmlFor="feeling">②気分 = Feeling</label><span>*</span> <br />
-        <textarea id="feeling" name="feeling" cols="62" rows="4" placeholder="それぞれの強さを%表記します(寄与が大きいものに着目するとよいです。)。" required></textarea>
+        <textarea id="feeling" name="feeling" cols="50" rows="4" placeholder="それぞれの強さを%表記します(寄与が大きいものに着目するとよいです。)。" required></textarea>
       </div>
       <div>
         <label htmlFor="schematic-idea">③自動思考 = Idea</label><span>*</span> <br />
-        <textarea id="schematic-idea" name="schematic-idea" cols="62" rows="4" placeholder="自動思考とは、経験などから生じた思考の型(スキーマ)から発生する思考です。" required></textarea>
+        <textarea id="schematic-idea" name="schematic-idea" cols="50" rows="4" placeholder="自動思考とは、経験などから生じた思考の型(スキーマ)から発生する思考です。" required></textarea>
       </div>
       <div>
         <label htmlFor="schematic-cause">④自動思考の根拠</label><span>*</span> <br />
-        <textarea id="schematic-cause" name="schematic-cause" cols="62" rows="4" placeholder="自分なりの根拠を書いてみます。" required></textarea>
+        <textarea id="schematic-cause" name="schematic-cause" cols="50" rows="4" placeholder="自分なりの根拠を書いてみます。" required></textarea>
       </div>
       <div>
         <label htmlFor="anti-thesis">⑤反証</label><span>*</span> <br />
-        <textarea id="anti-thesis" name="anti-thesis" cols="62" rows="4" placeholder="自分の経験に基づいた、多くの反証を見つけるとよいです。XXにとらわれすぎないようにします(XX: 指標など。)。" required></textarea>
+        <textarea id="anti-thesis" name="anti-thesis" cols="50" rows="4" placeholder="自分の経験に基づいた、多くの反証を見つけるとよいです。XXにとらわれすぎないようにします(XX: 指標など。)。" required></textarea>
       </div>
       <div>
         <label htmlFor="rational-idea">⑥適応的思考</label><span>*</span> <br />
-        <textarea id="rational-idea" name="rational-idea" cols="62" rows="4" placeholder="反証から、思考の偏りに気付き、バランスの良い思考を導き出します。" required></textarea>
+        <textarea id="rational-idea" name="rational-idea" cols="50" rows="4" placeholder="反証から、思考の偏りに気付き、バランスの良い思考を導き出します。" required></textarea>
       </div>
       <div>
         <label htmlFor="change">⑦気分の変化</label><span>*</span> <br />
-        <textarea id="change" name="change" cols="62" rows="4" placeholder="それぞれの強さを%表記します。" required></textarea>
+        <textarea id="change" name="change" cols="50" rows="4" placeholder="それぞれの強さを%表記します。" required></textarea>
       </div>
       <div>
         <label htmlFor="option">Option</label> <br />
-        <textarea id="option" name="option" cols="62" rows="4" placeholder="振り返ってみると、さらに俯瞰ができます。"></textarea>
+        <textarea id="option" name="option" cols="50" rows="4" placeholder="振り返ってみると、さらに俯瞰ができます。"></textarea>
       </div>
       <div>
         <input type="submit" value="送信" />
