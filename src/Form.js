ded3699527c3e27d43aa03867e8c27e1ce96d2de
solaris0051 <30798030+solaris0051@users.noreply.github.com>
2023-01-30 21:15:42 +0900

    wrap off

--- src/Form.js
+++ src/Form.js
@@ -13,42 +13,42 @@ export default function Form() {
       <br />
       <div>
         <label htmlFor="①状況">①状況 = Event<span>*</span></label>
-        <textarea id="①状況" name="①状況" cols="50" rows="4" wrap="hard" placeholder="発生タイミングは、②③が発生する直前です。それを叙事的(機械的)に書きます。" required></textarea>
+        <textarea id="①状況" name="①状況" cols="50" rows="4" wrap="off" placeholder="発生タイミングは、②③が発生する直前です。それを叙事的(機械的)に書きます。" required></textarea>
       </div>
       <br />
       <div>
         <label htmlFor="②気分">②気分 = Feeling<span>*</span></label>
-        <textarea id="②気分" name="②気分" cols="50" rows="4" wrap="hard" placeholder="それぞれの強さを%表記します(寄与が大きいものに着目するとよいです。)。" required></textarea>
+        <textarea id="②気分" name="②気分" cols="50" rows="4" wrap="off" placeholder="それぞれの強さを%表記します(寄与が大きいものに着目するとよいです。)。" required></textarea>
       </div>
       <br />
       <div>
         <label htmlFor="③自動思考">③自動思考 = Idea<span>*</span></label>
-        <textarea id="③自動思考" name="③自動思考" cols="50" rows="4" wrap="hard" placeholder="自動思考とは、経験などから生じた思考の型(スキーマ)から発生する思考です。" required></textarea>
+        <textarea id="③自動思考" name="③自動思考" cols="50" rows="4" wrap="off" placeholder="自動思考とは、経験などから生じた思考の型(スキーマ)から発生する思考です。" required></textarea>
       </div>
       <br />
       <div>
         <label htmlFor="④根拠">④根拠<span>*</span></label>
-        <textarea id="④根拠" name="④根拠" cols="50" rows="4" wrap="hard" placeholder="自分なりの根拠を書いてみます。" required></textarea>
+        <textarea id="④根拠" name="④根拠" cols="50" rows="4" wrap="off" placeholder="自分なりの根拠を書いてみます。" required></textarea>
       </div>
       <br />
       <div>
         <label htmlFor="⑤反証">⑤反証<span>*</span></label>
-        <textarea id="⑤反証" name="⑤反証" cols="50" rows="4" wrap="hard" placeholder="自分の経験に基づいた、多くの反証を見つけるとよいです。XXにとらわれすぎないようにします(XX: 指標など。)。" required></textarea>
+        <textarea id="⑤反証" name="⑤反証" cols="50" rows="4" wrap="off" placeholder="自分の経験に基づいた、多くの反証を見つけるとよいです。XXにとらわれすぎないようにします(XX: 指標など。)。" required></textarea>
       </div>
       <br />
       <div>
         <label htmlFor="⑥適応的思考">⑥適応的思考<span>*</span></label>
-        <textarea id="⑥適応的思考" name="⑥適応的思考" cols="50" rows="4" wrap="hard" placeholder="反証から、思考の偏りに気付き、バランスの良い思考を導き出します。" required></textarea>
+        <textarea id="⑥適応的思考" name="⑥適応的思考" cols="50" rows="4" wrap="off" placeholder="反証から、思考の偏りに気付き、バランスの良い思考を導き出します。" required></textarea>
       </div>
       <br />
       <div>
         <label htmlFor="⑦気分の変化">⑦気分の変化<span>*</span></label>
-        <textarea id="⑦気分の変化" name="⑦気分の変化" cols="50" rows="4" wrap="hard" placeholder="それぞれの強さを%表記します。" required></textarea>
+        <textarea id="⑦気分の変化" name="⑦気分の変化" cols="50" rows="4" wrap="off" placeholder="それぞれの強さを%表記します。" required></textarea>
       </div>
       <br />
       <div>
         <label htmlFor="振り返り">Option</label>
-        <textarea id="振り返り" name="振り返り" cols="50" rows="4" wrap="hard" placeholder="振り返ってみると、さらに俯瞰ができます。"></textarea>
+        <textarea id="振り返り" name="振り返り" cols="50" rows="4" wrap="off" placeholder="振り返ってみると、さらに俯瞰ができます。"></textarea>
       </div>
       <br />
       <div>
